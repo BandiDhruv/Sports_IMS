96ea7d860f2b398cd99dd693d087bb4bbe30db66
Dhruv Bandi <dhruvbandi03@gmail.com>
2023-10-30 11:45:36 +0530

    Backend created structurlly created models folder and data folder and deleted mongo file

--- frontend/src/components/Cricket/cricket.js
+++ frontend/src/components/Cricket/cricket.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./cricket.css"
+import Navbar from '../Navbar/navbar'
 
 const cricket = () => {
   return (
-    <div>cricket</div>
+    <div className="full-box">
+      <div className="navbarr"><Navbar /></div>
+      <div className="cricket-container">
+        <div className="card">
+          <div className="item-heading">
+
+          </div>
+          <div className="item-quantity">
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
