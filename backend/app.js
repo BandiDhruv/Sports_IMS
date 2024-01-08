a1259940223c8afaa36110853ee5329627abbb62
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-01-08 11:58:39 +0530

    links changed

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({credentials:true,origin:"http://localhost:3000"}));
+app.use(cors({credentials:true,origin:"https://sports-ims.vercel.app/"}));
 app.use(cookieParser()); 
 
 const databaseName = "logindetails"; 
