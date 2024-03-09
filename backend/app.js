fa63131c08e789cc483bbd68432a90e83d7413e0
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-03-09 14:10:08 +0530

    cors options updated

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,8 @@ const corsOptions = {
 };
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
-app.options(cors(corsOptions));
+// app.use(cors(corsOptions));
+// app.options(cors(corsOptions));
 app.use(cookieParser()); 
 app.use(session({
   secret:  'dhruv123',
