77d9cb45496e8d1f0dc315714296ecb75c941233
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-04-13 02:23:48 +0530

    / is added

--- frontend/src/components/AnotherComponent/AnotherComponent.js
+++ frontend/src/components/AnotherComponent/AnotherComponent.js
@@ -21,7 +21,7 @@ const AnotherComponent = () => {
   async function getDetails() {
     setLoading(true)
     await axios
-      .get(`${process.env.REACT_APP_BACKEND_IP}InventoryDetails`,{withCredentials: true})
+      .get(`${process.env.REACT_APP_BACKEND_IP}/InventoryDetails`,{withCredentials: true})
       .then((resp) => {
         setFetchData(resp.data);
         const modifiedData = Array.isArray(resp.data)
@@ -41,7 +41,7 @@ const AnotherComponent = () => {
   async function getStatus() {
     const userEmail = localStorage.getItem("userEmail");
     await axios
-      .get(`${process.env.REACT_APP_BACKEND_IP}get-status/${userEmail}`,{withCredentials: true})
+      .get(`${process.env.REACT_APP_BACKEND_IP}/get-status/${userEmail}`,{withCredentials: true})
       .then((resp) => {
         setStatusData(resp.data.details);
       })
@@ -61,7 +61,7 @@ const AnotherComponent = () => {
 
       await axios
         .post(
-          `${process.env.REACT_APP_BACKEND_IP}reserve`,
+          `${process.env.REACT_APP_BACKEND_IP}/reserve`,
           {
             sportName: sport,
             userEmail: userEmail,
